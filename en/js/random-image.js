3dab46180d5787314c24a10078a025de64cf63b7
salem <salemchniguir2@gmail.com>
2022-04-13 03:13:58 +0100

    edit hotels

--- en/js/random-image.js
+++ en/js/random-image.js
@@ -1,51 +1,252 @@
 window.onload = function () {
-  document.getElementById('club-meninx').style.backgroundImage = 'url(../hotels/club-meninx-3e/1.jpg)';
-  var rndImagesClubMeninx = [
-    'url(../hotels/club-meninx-3e/1.jpg)',
-    'url(../hotels/club-meninx-3e/2.jpg)',
-    'url(../hotels/club-meninx-3e/3.jpg)',
-    'url(../hotels/club-meninx-3e/4.jpg)',
-    'url(../hotels/club-meninx-3e/5.jpg)',
-    'url(../hotels/club-meninx-3e/6.jpg)'
+  document.getElementById('1-club-meninx').style.backgroundImage = 'url(../hotels/1-club-meninx-3e/1.jpg)';
+  var rndImages1 = [
+    'url(../hotels/1-club-meninx-3e/1.jpg)',
+    'url(../hotels/1-club-meninx-3e/2.jpg)',
+    'url(../hotels/1-club-meninx-3e/3.jpg)',
+    'url(../hotels/1-club-meninx-3e/4.jpg)',
+    'url(../hotels/1-club-meninx-3e/5.jpg)',
+    'url(../hotels/1-club-meninx-3e/6.jpg)'
   ]
 
   setInterval(function () {
-    var i = Math.floor(Math.random() * rndImagesClubMeninx.length);
-    document.getElementById('club-meninx').style.backgroundImage =  rndImagesClubMeninx[i];
+    var i = Math.floor(Math.random() * rndImages1.length);
+    document.getElementById('1-club-meninx').style.backgroundImage =  rndImages1[i];
   }, 3000) 
 
 
-  document.getElementById('holiday-beach-4e').style.backgroundImage = 'url(../hotels/holiday-beach-4e/1.jpg)';
-  var rndImagesHolidayBeach = [
-    'url(../hotels/holiday-beach-4e/1.jpg)',
-    'url(../hotels/holiday-beach-4e/2.jpg)',
-    'url(../hotels/holiday-beach-4e/3.jpg)',
-    'url(../hotels/holiday-beach-4e/4.jpg)',
-    'url(../hotels/holiday-beach-4e/5.jpg)',
-    'url(../hotels/holiday-beach-4e/6.jpg)'
+
+
+
+
+
+
+
+
+
+
+
+  document.getElementById('2-holiday-beach-4e').style.backgroundImage = 'url(../hotels/2-holiday-beach-4e/1.jpg)';
+  var rndImages2 = [
+    'url(../hotels/2-holiday-beach-4e/1.jpg)',
+    'url(../hotels/2-holiday-beach-4e/2.jpg)',
+    'url(../hotels/2-holiday-beach-4e/3.jpg)',
+    'url(../hotels/2-holiday-beach-4e/4.jpg)',
+    'url(../hotels/2-holiday-beach-4e/5.jpg)',
+    'url(../hotels/2-holiday-beach-4e/6.jpg)'
   ]
 
   setInterval(function () {
-    var i = Math.floor(Math.random() * rndImagesHolidayBeach.length);
-    document.getElementById('holiday-beach-4e').style.backgroundImage =  rndImagesHolidayBeach[i];
+    var i = Math.floor(Math.random() * rndImages2.length);
+    document.getElementById('2-holiday-beach-4e').style.backgroundImage =  rndImages2[i];
   }, 2000) // Change 2000 to 10000.
-  document.getElementById('joya-paradise-4e').style.backgroundImage = 'url(../hotels/joya-paradise-4e/1.jpg)';
-  var rndImagesJoyaParadise = [
-    'url(../hotels/joya-paradise-4e/1.jpg)',
-    'url(../hotels/joya-paradise-4e/2.jpg)',
-    'url(../hotels/joya-paradise-4e/3.jpg)',
-    'url(../hotels/joya-paradise-4e/4.jpg)',
-    'url(../hotels/joya-paradise-4e/5.jpg)',
-    'url(../hotels/joya-paradise-4e/6.jpg)',
-    'url(../hotels/joya-paradise-4e/7.jpg)',
-    'url(../hotels/joya-paradise-4e/8.jpg)'
+
+
+
+
+
+
+
+
+
+  document.getElementById('3-joya-paradise-4e').style.backgroundImage = 'url(../hotels/3-joya-paradise-4e/1.jpg)';
+  var rndImages3 = [
+    'url(../hotels/3-joya-paradise-4e/1.jpg)',
+    'url(../hotels/3-joya-paradise-4e/2.jpg)',
+    'url(../hotels/3-joya-paradise-4e/3.jpg)',
+    'url(../hotels/3-joya-paradise-4e/4.jpg)',
+    'url(../hotels/3-joya-paradise-4e/5.jpg)',
+    'url(../hotels/3-joya-paradise-4e/6.jpg)',
+    'url(../hotels/3-joya-paradise-4e/7.jpg)',
+    'url(../hotels/3-joya-paradise-4e/8.jpg)'
   ]
 
   setInterval(function () {
-    var i = Math.floor(Math.random() * rndImagesJoyaParadise.length);
-    document.getElementById('joya-paradise-4e').style.backgroundImage =  rndImagesJoyaParadise[i];
+    var i = Math.floor(Math.random() * rndImages3.length);
+    document.getElementById('3-joya-paradise-4e').style.backgroundImage =  rndImages3[i];
   }, 4000) // Change 2000 to 10000.
 
 
+
+
+
+
+
+
+
+
+
+
+
+  document.getElementById('4-dar-djerba-zahra-3e').style.backgroundImage = 'url(../hotels/4-dar-djerba-zahra-3e/1.jpg)';
+  var rndImages4 = [
+    'url(../hotels/4-dar-djerba-zahra-3e/1.png)'
+    ]
+
+  setInterval(function () {
+    var i = Math.floor(Math.random() * rndImages4.length);
+    document.getElementById('4-dar-djerba-zahra-3e').style.backgroundImage =  rndImages4[i];
+  }, 3400) // Change 2000 to 10000.
+
+
+
+
+
+
+
+
+
+
+  document.getElementById('5-dar-djerba-narjess-4e').style.backgroundImage = 'url(../hotels/5-dar-djerba-narjess-4e/1.jpg)';
+  var rndImages5 = [
+    'url(../hotels/5-dar-djerba-narjess-4e/1.png)'
+    ]
+
+  setInterval(function () {
+    var i = Math.floor(Math.random() * rndImages5.length);
+    document.getElementById('5-dar-djerba-narjess-4e').style.backgroundImage =  rndImages5[i];
+  }, 2300) // Change 2000 to 10000.
   
+
+
+
+
+
+  document.getElementById('6-ilbastor-mehari-djerba-4e').style.backgroundImage = 'url(../hotels/6-ilbastor-mehari-djerba-4e/1.jpg)';
+  var rndImages6 = [
+    'url(../hotels/6-ilbastor-mehari-djerba-4e/1.png)'
+    ]
+
+  setInterval(function () {
+    var i = Math.floor(Math.random() * rndImages6.length);
+    document.getElementById('6-ilbastor-mehari-djerba-4e').style.backgroundImage =  rndImages6[i];
+  }, 3400) // Change 2000 to 10000.
+
+
+
+
+
+
+
+
+  document.getElementById('7-sidi-mansour-4e').style.backgroundImage = 'url(../hotels/7-sidi-mansour-4e/1.jpg)';
+  var rndImages7 = [
+    'url(../hotels/7-sidi-mansour-4e/1.jpg)',
+    'url(../hotels/7-sidi-mansour-4e/2.jpg)',
+    'url(../hotels/7-sidi-mansour-4e/3.jpg)',
+    'url(../hotels/7-sidi-mansour-4e/4.jpg)',
+    'url(../hotels/7-sidi-mansour-4e/5.jpg)',
+    'url(../hotels/7-sidi-mansour-4e/6.jpg)',
+    'url(../hotels/7-sidi-mansour-4e/7.jpg)',
+    'url(../hotels/7-sidi-mansour-4e/8.jpg)',
+    'url(../hotels/7-sidi-mansour-4e/9.jpg)'
+    
+    ]
+
+  setInterval(function () {
+    var i = Math.floor(Math.random() * rndImages7.length);
+    document.getElementById('7-sidi-mansour-4e').style.backgroundImage =  rndImages7[i];
+  }, 2000) // Change 2000 to 10000.
+
+
+
+
+
+
+
+
+  document.getElementById('8-sunconnect-4e').style.backgroundImage = 'url(../hotels/8-sunconnect-4e/1.jpg)';
+  var rndImages8 = [
+    'url(../hotels/8-sunconnect-4e/1.jpg)',
+    'url(../hotels/8-sunconnect-4e/2.jpg)',
+    'url(../hotels/8-sunconnect-4e/3.jpg)',
+    'url(../hotels/8-sunconnect-4e/4.jpg)',
+    'url(../hotels/8-sunconnect-4e/5.jpg)',
+    'url(../hotels/8-sunconnect-4e/6.jpg)',
+    'url(../hotels/8-sunconnect-4e/7.jpg)',
+    'url(../hotels/8-sunconnect-4e/8.jpg)',
+    'url(../hotels/8-sunconnect-4e/9.jpg)',
+    'url(../hotels/8-sunconnect-4e/10.jpg)',
+    'url(../hotels/8-sunconnect-4e/11.jpg)',
+    'url(../hotels/8-sunconnect-4e/12.jpg)'
+    
+    ]
+
+  setInterval(function () {
+    var i = Math.floor(Math.random() * rndImages8.length);
+    document.getElementById('8-sunconnect-4e').style.backgroundImage =  rndImages8[i];
+  }, 3400) // Change 2000 to 10000.
+
+
+
+
+
+
+
+  document.getElementById('9-vincci-helios-4e').style.backgroundImage = 'url(../hotels/9-vincci-helios-4e/1.jpg)';
+  var rndImages9 = [
+    'url(../hotels/9-vincci-helios-4e/2.jpg)',
+    'url(../hotels/9-vincci-helios-4e/3.jpg)',
+    'url(../hotels/9-vincci-helios-4e/4.jpg)',
+    'url(../hotels/9-vincci-helios-4e/6.jpg)',
+    'url(../hotels/9-vincci-helios-4e/7.jpg)',
+    'url(../hotels/9-vincci-helios-4e/8.jpg)',
+    'url(../hotels/9-vincci-helios-4e/9.jpg)',
+    'url(../hotels/9-vincci-helios-4e/10.jpg)'
+    
+    ]
+
+  setInterval(function () {
+    var i = Math.floor(Math.random() * rndImages9.length);
+    document.getElementById('9-vincci-helios-4e').style.backgroundImage =  rndImages9[i];
+  }, 3000) // Change 2000 to 10000.
+
+
+
+
+
+
+
+
+  document.getElementById('10-welcome-meridiana-4e').style.backgroundImage = 'url(../hotels/10-welcome-meridiana-4e/1.jpg)';
+  var rndImages10 = [
+    'url(../hotels/10-welcome-meridiana-4e/1.jpg)',
+    'url(../hotels/10-welcome-meridiana-4e/2.jpg)',
+    'url(../hotels/10-welcome-meridiana-4e/3.jpg)',
+    'url(../hotels/10-welcome-meridiana-4e/4.jpg)',
+    'url(../hotels/10-welcome-meridiana-4e/5.jpg)',
+    'url(../hotels/10-welcome-meridiana-4e/6.jpg)'
+    
+    ]
+
+  setInterval(function () {
+    var i = Math.floor(Math.random() * rndImages10.length);
+    document.getElementById('10-welcome-meridiana-4e').style.backgroundImage =  rndImages10[i];
+  }, 3400) // Change 2000 to 10000.
+
+
+
+
+
+
+  document.getElementById('11-yadis-4e').style.backgroundImage = 'url(../hotels/11-yadis-4e/1.jpg)';
+  var rndImages11 = [
+    'url(../hotels/11-yadis-4e/1.jpg)',
+    'url(../hotels/11-yadis-4e/2.jpg)',
+    'url(../hotels/11-yadis-4e/3.jpg)',
+    'url(../hotels/11-yadis-4e/4.jpg)',
+    'url(../hotels/11-yadis-4e/5.jpg)',
+    'url(../hotels/11-yadis-4e/6.jpg)',
+    'url(../hotels/11-yadis-4e/7.jpg)',
+    'url(../hotels/11-yadis-4e/8.jpg)',
+    'url(../hotels/11-yadis-4e/9.jpg)',
+    'url(../hotels/11-yadis-4e/10.jpg)'
+    ]
+
+  setInterval(function () {
+    var i = Math.floor(Math.random() * rndImages11.length);
+    document.getElementById('11-yadis-4e').style.backgroundImage =  rndImages11[i];
+  }, 3000) // Change 2000 to 10000.
+
+
 }
